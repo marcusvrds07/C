ebbe31a84fa1feecd742fe25f9734c9bd82c9371
Marcus Vinicius <marcusvrds07@gmail.com>
2024-08-26 14:10:10 -0300

    Exercicio 10

--- TDE 1/main.c
+++ TDE 1/main.c
@@ -314,62 +314,87 @@
 
 //Exercicio 9
 
+// #include <stdio.h>
+
+//     float soma(float num1, float num2) {
+//         return num1 + num2;
+//     }
+//     float subtracao(float num1, float num2) {
+//         return num1 - num2;
+//     }
+//     float divisao(float num1, float num2) {
+//         return num1 / num2;
+//     }
+//     float multiplicacao(float num1, float num2) {
+//         return num1 * num2;
+//     }
+// int main(void) {
+//     float num1, num2, resultado;
+//     int operacao;
+
+//     printf("Digite um número: ");
+//     scanf("%f", &num1);
+
+//     printf("\nDigite outro número: ");
+//     scanf("%f", &num2);
+
+//     printf("\nEscolha a operação desejada:\n");
+//     printf("1 - Soma\n");
+//     printf("2 - Subtração\n");
+//     printf("3 - Divisão\n");
+//     printf("4 - multiplicação\n");
+//     scanf("%d", &operacao);
+
+//     switch (operacao)
+//     {
+//     case 1:
+//         resultado = soma(num1,num2);
+//         printf("Resultado de %.2f+%.2f=%.2f\n\n", num1, num2, resultado);
+//         break;
+//     case 2:
+//         resultado = subtracao(num1,num2);
+//         printf("Resultado de %.2f-%.2f=%.2f\n\n", num1, num2, resultado);
+//         break;
+//     case 3:
+//         if(num2 > 0) {
+//             resultado = divisao(num1,num2);
+//             printf("Resultado de %.2f/%.2f=%.2f\n\n", num1, num2, resultado);
+//         }
+//         else {
+//             printf("Divisão por 0 não é permitido!\n\n");
+//         }
+//         break;
+//     case 4:
+//         resultado = multiplicacao(num1,num2);
+//         printf("Resultado de %.2fx%.2f=%.2f\n\n", num1, num2, resultado);
+//         break;
+//     default:
+//         break;
+//     }
+//     return 0;
+// }
+
+//Exercicio 10
+
 #include <stdio.h>
 
-    float soma(float num1, float num2) {
-        return num1 + num2;
+#define M2 7
+    void resultado(int limite) {
+        int result = 0;
+        
+        for(int i = 0; (i * M2) <= limite; i++) {
+                result = i * M2;
+                printf("%d x %d = %d\n", i, M2, result);
+        }
     }
-    float subtracao(float num1, float num2) {
-        return num1 - num2;
-    }
-    float divisao(float num1, float num2) {
-        return num1 / num2;
-    }
-    float multiplicacao(float num1, float num2) {
-        return num1 * num2;
-    }
+
 int main(void) {
-    float num1, num2, resultado;
-    int operacao;
+    int limite;
 
-    printf("Digite um número: ");
-    scanf("%f", &num1);
+    printf("Digite o Limite do resultado da tabuada: ");
+    scanf("%d", &limite);
 
-    printf("\nDigite outro número: ");
-    scanf("%f", &num2);
+    resultado(limite);
 
-    printf("\nEscolha a operação desejada:\n");
-    printf("1 - Soma\n");
-    printf("2 - Subtração\n");
-    printf("3 - Divisão\n");
-    printf("4 - multiplicação\n");
-    scanf("%d", &operacao);
-
-    switch (operacao)
-    {
-    case 1:
-        resultado = soma(num1,num2);
-        printf("Resultado de %.2f+%.2f=%.2f\n\n", num1, num2, resultado);
-        break;
-    case 2:
-        resultado = subtracao(num1,num2);
-        printf("Resultado de %.2f-%.2f=%.2f\n\n", num1, num2, resultado);
-        break;
-    case 3:
-        if(num2 > 0) {
-            resultado = divisao(num1,num2);
-            printf("Resultado de %.2f/%.2f=%.2f\n\n", num1, num2, resultado);
-        }
-        else {
-            printf("Divisão por 0 não é permitido!\n\n");
-        }
-        break;
-    case 4:
-        resultado = multiplicacao(num1,num2);
-        printf("Resultado de %.2fx%.2f=%.2f\n\n", num1, num2, resultado);
-        break;
-    default:
-        break;
-    }
     return 0;
 }
