40400190ac1aa8c6cfb881f4bb8fe4022af5f66b
Marcus Vinicius <marcusvrds07@gmail.com>
2024-09-19 14:20:58 -0300

    Exercicio 16

--- TDE 1/main.c
+++ TDE 1/main.c
@@ -376,25 +376,231 @@
 
 //Exercicio 10
 
-#include <stdio.h>
+// #include <stdio.h>
 
-#define M2 7
-    void resultado(int limite) {
-        int result = 0;
+// #define M2 7
+//     void resultado(int limite) {
+//         int result = 0;
         
-        for(int i = 0; (i * M2) <= limite; i++) {
-                result = i * M2;
-                printf("%d x %d = %d\n", i, M2, result);
-        }
-    }
+//         for(int i = 0; (i * M2) <= limite; i++) {
+//                 result = i * M2;
+//                 printf("%d x %d = %d\n", i, M2, result);
+//         }
+//     }
 
-int main(void) {
-    int limite;
+// int main(void) {
+//     int limite;
 
-    printf("Digite o Limite do resultado da tabuada: ");
-    scanf("%d", &limite);
+//     printf("Digite o Limite do resultado da tabuada: ");
+//     scanf("%d", &limite);
 
-    resultado(limite);
+//     resultado(limite);
 
-    return 0;
-}
+//     return 0;
+// }
+
+//Exercicio 11
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int maior(int num1, int num2, int num3);
+
+// int main(void) {
+//     int num1, num2, num3;
+//     char entrada[30];
+//     bool valid = false;
+    
+    // do {
+    //     printf("Digite 3 numeros da seguinte forma (01 02 03): ");
+    //     fgets(entrada, sizeof(entrada), stdin);
+    // if(sscanf(entrada, "%d %d %d", &num1, &num2, &num3) == 3) {
+    //         valid = true;
+    //     }
+    //     else {
+    //         printf("\nERROR: Certifique-se de fornecer um formato válido.\n\n");
+    //     }
+    // } while (valid == false);
+
+
+//     int resultado = maior(num1, num2, num3);
+//     printf("O maior número é %d", resultado);
+
+//     return 0;
+// }
+
+// int maior(int num1, int num2, int num3) {
+//     if(num1 > num2 && num1 > num3) {
+//         return num1;
+//     }
+//     else if(num2 > num3 && num2 > num1) {
+//         return num2;
+//     }
+//     else {
+//         return num3;
+//     }
+// }
+
+//Exercicio 12
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdbool.h> // inclui boleanos
+// #include <ctype.h> // inclui toLower
+
+// float reajustar(float salario, char nivelE[30]);
+
+// int main(void) {
+//     float salario;
+//     char nivelE[30], entrada[30];
+//     bool valid = false;
+    
+//     do {
+//         printf("Digite o seu salario: ");
+//         fgets(entrada, sizeof(entrada), stdin);
+//         if(sscanf(entrada, "%f", &salario) == 1 && salario > 0) {
+//             valid = true;
+//         }
+//         else {
+//             printf("\nERROR: Certifique-se de fornecer um salario válido.\n\n");
+//         }
+//     } while(valid == false);
+
+//     valid = false;
+//     do {
+//         printf("Digite o seu nível de experiencia (senior, pleno ou junior): ");
+//         scanf(" %19s", nivelE);
+
+//         int len = strlen(nivelE);
+
+//         for(int i = 0; i < len; i++) {
+//             nivelE[i] = tolower(nivelE[i]);
+//         }
+
+//         if(strcmp(nivelE, "senior") == 0 || strcmp(nivelE, "pleno") == 0 || strcmp(nivelE, "junior") == 0) {
+//             valid = true;
+//         }
+//     } while(valid == false);
+
+//     float resultado = reajustar(salario, nivelE);
+
+//     printf("O seu novo salario é %.2f", resultado);
+
+//     return 0;
+// }
+
+// float reajustar(float salario, char nivelE[30]) {
+//     if(strcmp(nivelE, "senior") == 0) {
+//         return salario * 1.05;
+//     }
+//     else if (strcmp(nivelE, "pleno") == 0) {
+//         return salario * 1.1;
+//     }
+//     else {
+//         return salario * 1.15;
+//     }
+// }
+
+// Exercicio 13
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// float somar(float num1, float num2, float num3);
+// float media(float resultado);
+
+// int main(void) {
+//     float num1, num2, num3;
+//     char entrada[30];
+//     bool valid = false;
+
+//     do {
+//         printf("Digite 3 números no seguinte formato (01 02 03): ");
+//         fgets(entrada, sizeof(entrada), stdin);
+
+//         if((sscanf(entrada, "%f %f %f", &num1, &num2, &num3) == 3) && (num1 > 0 && num2 > 0 && num3 > 0)) {
+//             valid = true;
+//         }
+//         else {
+//             printf("\nERROR: Certifique-se de fornecer 3 números válidos e no formato certo.\n\n");
+//         }
+//     } while(valid == false);
+
+//     float resultado = media(somar(num1, num2, num3));
+//     printf("Resultado = %.2f", resultado);
+
+//     return 0;
+// }
+
+// float somar(float num1, float num2, float num3) {
+//     return num1 + num2 + num3;
+// }
+
+// float media(float resultado) {
+//     return resultado / 3;
+// }
+
+//Exercicio 14
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int pontosTotais(int vitorias, int empates);
+// float media(float resultado, int jogosTotais);
+
+// int main(void) {
+//     int vitorias, empates, derrotas, jogosTotais;
+//     bool valid = false;
+
+//     do {
+//         char entrada[30];
+
+//         printf("Digite o número vitorias, empates e derrotas do seu time, seguindo esse formato (vt-et-dt): ");
+//         fgets(entrada, sizeof(entrada), stdin);
+//         if((sscanf(entrada, "%d-%d-%d", &vitorias, &empates, &derrotas) == 3) && (vitorias >= 0 && empates >= 0 && derrotas >= 0)) {
+//             valid = true;
+//         }
+//         else {
+//             printf("\nERROR: Certifique-se de fornecer 3 números válidos e no formato certo.\n\n");
+//         }
+
+
+//     } while(valid == false);
+    
+//     jogosTotais = vitorias + empates + derrotas;
+//     float total = pontosTotais(vitorias, empates);
+//     float mediaFinal = media(total, jogosTotais);
+
+//     printf("\n\nEm %d jogos o seu time fez %d pontos, tendo uma média de %.2f pontos por partida.\n\n", jogosTotais, total, mediaFinal);
+
+//     return 0;
+// }
+
+// int pontosTotais(int vitorias, int empates) {
+//     return (vitorias * 3) + empates;
+// }
+// float media(float resultado, int jogosTotais) {
+//     return resultado / jogosTotais;
+// }
+
+//Exercicio 15
+
+// #include <stdio.h>
+
+// int main(void) {
+//     int a = 25;
+//     int *pa = &a; // ponteiro PA recebe o endereço de A
+//     int b = *pa + a; // conteudo do endereço de pa = 25 + a = 25
+//     printf("%d %p %p %d %d %p\n", a, pa, &a, *pa, b, &b);
+
+//     //a) O programa irá dar erro pois o % para ponteiro, endereço é %p
+//     // o % do conteudo do endereço (*pa) vai depender do tipo de variavel que o ponteiro ta fazendo referencia, nesse caso o ponteiro ta referindo ao int a que é um inteiro e o % é o %d, mas se fosse float usariamos %f
+
+//     //b)
+
+//     // O printf vai exibir em ordem o valor de A que é 25, depois vai exibir o enderço de a que está guardado no ponteiro PA, vai exibir o endreço de A novamente, o conteudo do enderço que ta guardado em PA, vai exibir o resultado de B que é 50 e depois o endereço de B.
+
+//     return 0;
+// }
+
+//Exercicio 16
